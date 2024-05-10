6ac717dbfbaac7c889419d2bb2e76c691107bf72
Gergely Varga <gergelyv@amazon.com>
2024-05-10 15:30:30 +0800

    feat(infra): add ws connections table

--- demo/infra/src/application/data/app-data-layer.ts
+++ demo/infra/src/application/data/app-data-layer.ts
@@ -1,14 +1,16 @@
 /*! Copyright [Amazon.com](http://amazon.com/), Inc. or its affiliates. All Rights Reserved.
 PDX-License-Identifier: Apache-2.0 */
 import { isDevStage, stageAwareRemovalPolicy } from '@aws/galileo-cdk/lib/common';
-import { AttributeType, BillingMode, Table } from 'aws-cdk-lib/aws-dynamodb';
+import { AttributeType, BillingMode, ITable, Table } from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export interface AppDataLayerProps {}
 
 export class AppDataLayer extends Construct {
-  readonly datastore: Table;
+  readonly datastore: ITable;
   readonly gsiIndexName = 'GSI1';
+
+  readonly wsConnections: ITable;
 
   constructor(scope: Construct, id: string, _props?: AppDataLayerProps) {
     super(scope, id);
@@ -16,7 +18,7 @@ export class AppDataLayer extends Construct {
     const dev = isDevStage(this);
 
     // Create the datastore for the CRUD operations
-    this.datastore = new Table(this, 'Datastore', {
+    const datastore = new Table(this, 'Datastore', {
       partitionKey: {
         name: 'PK',
         type: AttributeType.STRING,
@@ -30,7 +32,7 @@ export class AppDataLayer extends Construct {
       pointInTimeRecovery: !dev,
     });
 
-    this.datastore.addGlobalSecondaryIndex({
+    datastore.addGlobalSecondaryIndex({
       indexName: this.gsiIndexName,
       partitionKey: {
         name: 'GSI1PK',
@@ -40,6 +42,18 @@ export class AppDataLayer extends Construct {
         name: 'GSI1SK',
         type: AttributeType.STRING,
       },
+    });
+
+    this.datastore = datastore;
+
+    this.wsConnections = new Table(this, 'WsConnection', {
+      partitionKey: {
+        name: 'PK',
+        type: AttributeType.STRING,
+      },
+      billingMode: BillingMode.PAY_PER_REQUEST,
+      removalPolicy: stageAwareRemovalPolicy(this),
+      pointInTimeRecovery: !dev,
     });
   }
 }
